e6e1fa8a7e799591f0567b972fbbbf11fc69e353
Gordon Li <gordonli08@gmail.com>
2020-07-27 18:01:20 -0400

    Kindergarten Garden

---
+++ kindergarten-garden/kindergarten_garden.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+plant_lookup = {
+    'C':'Clover',
+    'G':'Grass',
+    'R':'Radishes',
+    'V':'Violets'
+}
+
+defaultStudents = [
+    'Alice', 'Bob', 'Charlie', 'David',
+    'Eve', 'Fred', 'Ginny', 'Harriet',
+    'Ileana', 'Joseph', 'Kincaid', 'Larry'
+]
+
+class Garden:
+
+    def __init__(self, diagram, students=None):
+        if not students:
+            students = deque(defaultStudents)
+        else:
+            students = deque(sorted(students))
+        
+        self.student_plants = {}
+
+        row_1, row_2 = diagram.splitlines()
+        plants = [row_1[index:index+2]+row_2[index:index+2] for index in range(0,len(row_1),2)]
+
+        for plantgroup in plants:
+            person = students.popleft()
+            self.student_plants[person] = [plant_lookup[plant] for plant in plantgroup]
+
+    def plants(self,student):
+        return self.student_plants[student]
