4da90ab7fcbb10bbe1cc0eef399f9c874c1e60bc
Gordon Li <gordonli08@gmail.com>
2020-08-22 19:49:34 -0400

    Clock

---
+++ clock/clock.py
@@ -0,0 +1,17 @@
+class Clock:
+    def __init__(self, hour, minute):
+        hr, mnt = divmod(minute,60)
+        self.hour = (hour + hr) % 24
+        self.minute = mnt
+
+    def __repr__(self):
+        return "{:02}:{:02}".format(self.hour,self.minute)
+
+    def __eq__(self, other):
+        return self.hour == other.hour and self.minute == other.minute
+
+    def __add__(self, minutes):
+        return Clock(self.hour, self.minute + minutes)
+
+    def __sub__(self, minutes):
+        return Clock(self.hour, self.minute - minutes)
