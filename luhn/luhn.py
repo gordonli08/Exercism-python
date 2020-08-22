a0d5b5002581e41836dde03a5cacb92eddfc146e
Gordon Li <gordonli08@gmail.com>
2020-08-22 15:52:37 -0400

    luhn fix

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -6,11 +6,13 @@ class Luhn:
         if len(self.card_num) < 2 or not self.card_num.isnumeric():
             return False
         
-        list_nums = [int(num) for num in self.card_num if num.isnumeric()]
+        list_nums = [int(num) for num in self.card_num]
         list_nums.reverse()
 
         for index, num in enumerate(list_nums):
             if index%2 == 1:
-                list_nums[index] = num*2-9 if num*2 > 9 else num*2
+                list_nums[index] = num*2
+                if list_nums[index] > 9:
+                    list_nums[index] -= 9
         
         return sum(list_nums)%10 == 0
